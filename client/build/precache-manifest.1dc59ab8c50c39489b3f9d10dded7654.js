bd080e5b9932b29d91fb507d2accfd2eeef099b4
Igor Baksht <igorexa81@gmail.com>
2019-08-21 21:39:05 -0600

    new changes

---
+++ client/build/precache-manifest.1dc59ab8c50c39489b3f9d10dded7654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2b273208967731cc602",
+    "url": "/static/js/main.bdf68a94.chunk.js"
+  },
+  {
+    "revision": "60bc303a424078cf332b",
+    "url": "/static/js/2.a9bf0899.chunk.js"
+  },
+  {
+    "revision": "e2b273208967731cc602",
+    "url": "/static/css/main.fa2e7d3e.chunk.css"
+  },
+  {
+    "revision": "29b80ec246c30567002cf32a5f7109af",
+    "url": "/index.html"
+  }
+];
